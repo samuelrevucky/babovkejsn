cea77ebe50c092e5fb5e53fb5b7a484fb6881225
Samuel <revucky2@uniba.sk>
2024-04-01 21:08:23 +0200

    Modify register page template
    No functionality of the form

--- client/app/(common)/layout.tsx
+++ client/app/(common)/layout.tsx
@@ -12,32 +12,37 @@ export default function Layout(
   }>) {
     return (
       <>
-      <header className="bg-white">
-        <nav className="mx-auto flex flex-col lg:flex-row max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-            <div className="flex lg:flex-1">
-            <a href="/home" className="-m-4 hidden lg:block">
-            <div className="relative flex  items-top justify-center white pb-5">
-              <Image
-                className="relative pt-3"
-                src="/text_logo.png"
-                alt="Babovkejsn Logo"
-                width={200}
-                height={37}
-                priority
-              />
-            </div>
-            </a>
-            </div>
-            <div className="flex gap-x-12">
-              <NavItem href="/home">Domov</NavItem>
-              <NavItem href="/products">Produkty</NavItem>
-              <NavItem href="/order">Objednať</NavItem>
-            </div>
-            <div className="lg:flex lg:flex-1 lg:justify-end">
-              <NavItem href="/login">Prihlásiť sa <span aria-hidden="true">&rarr;</span></NavItem>
-            </div>
-        </nav>
-        </header>
+        <div className="fixed top-0 left-0 z-10 right-0 bg-white shadow-md">
+          <header>
+            <nav className="mx-auto flex flex-col lg:flex-row max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <div className="flex lg:flex-1">
+                <a href="/home" className="-m-4 hidden lg:block">
+                <div className="relative flex  items-top justify-center white pb-5">
+                  <Image
+                    className="relative pt-3"
+                    src="/text_logo.png"
+                    alt="Babovkejsn Logo"
+                    width={200}
+                    height={37}
+                    priority
+                  />
+                </div>
+                </a>
+                </div>
+                <div className="flex gap-x-12">
+                  <NavItem href="/home">Domov</NavItem>
+                  <NavItem href="/products">Produkty</NavItem>
+                  <NavItem href="/order">Objednať</NavItem>
+                </div>
+                <div className="lg:flex lg:flex-1 lg:justify-end">
+                  <NavItem href="/login">Prihlásiť sa <span aria-hidden="true">&rarr;</span></NavItem>
+                </div>
+            </nav>
+          </header>
+        </div>
+        <div className="p-10">
+         &nbsp;
+        </div>
         {children}
       </>
     );
