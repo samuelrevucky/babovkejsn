9103fdb29864c21f94946704a86a0619742985dc
Samuel <revucky2@uniba.sk>
2024-04-04 11:22:29 +0200

    Add forgot password page

--- client/app/(common)/forgot_password/page.tsx
+++ client/app/(common)/forgot_password/page.tsx
@@ -1,10 +1,10 @@
-export default function Login() {
+export default function ForgotPasswd() {
     return (
       <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Prihláste sa do svojho účtu
+              Zabudli ste svoje heslo? Nevadí.
             </h2>
           </div>
   
@@ -12,7 +12,7 @@ export default function Login() {
             <form className="space-y-6" action="#" method="POST">
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email
+                  Stačí, keď sem napíšete email ku vášmu kontu
                 </label>
                 <div className="mt-2">
                   <input
@@ -27,44 +27,14 @@ export default function Login() {
               </div>
   
               <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Heslo
-                  </label>
-                  <div className="text-sm">
-                    <a href="/forgot_password" className="font-semibold text-yellow-500 hover:text-yellow-400">
-                      Zabudli ste heslo?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-yellow-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Prihlásiť
+                  Odoslať
                 </button>
               </div>
             </form>
-  
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Nemáte účet?{' '}
-              <a href="/register" className="font-semibold leading-6 text-yellow-500 hover:text-yellow-400">
-                Zaregistrujte sa
-              </a>
-            </p>
           </div>
         </div>
       </>
