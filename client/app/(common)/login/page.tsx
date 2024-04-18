3af814ea0ab822b79fda4dc9cc2555943d144272
Samuel <revucky2@uniba.sk>
2024-04-18 20:33:18 +0200

    Fix bug in login form

--- client/app/(common)/login/page.tsx
+++ client/app/(common)/login/page.tsx
@@ -114,6 +114,7 @@ export default function Login() {
                   className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-500 sm:text-sm sm:leading-6"
                 />
                 <button
+                  type="button"
                   className="absolute inset-y-0 right-0 flex items-center px-4 text-gray-600"
                   onClick={togglePasswordVisibility}
                 >
